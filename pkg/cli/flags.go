0bd8e21c4afe75550f18098bbbc90210f295af7a
taku-k <taakuu19@gmail.com>
2017-08-17 18:43:37 +0900

    Add importCmd flags

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -12,6 +12,13 @@ func init() {
 		f.BoolVar(&cfg.EnableTracer, "--enable-tracer", cfg.EnableTracer, "Flag of enabling tracer.")
 	}
 
+	{
+		f := importCmd.Flags()
+
+		f.StringVar(&cfg.Host, "host", cfg.Host, "ipdrawer server host.")
+		f.StringVar(&cfg.Port, "port", cfg.Port, "ipdrawer server port.")
+	}
+
 	rootCmd.AddCommand(
 		startCmd,
 		importCmd,
